575420d4f5650763d41add99950aabba7c04a645
Safenorsound <ijroback@gmail.com>
2018-08-03 10:57:14 -0700

    Adding x and y vectors for the ball.

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,32 +1,35 @@
-import {SVG_NS} from '../settings';
+import { SVG_NS } from "../settings";
 
 // Ball.js
 export default class Ball {
-    constructor(radius, boardWidth, boardHeight) {
-      this.radius = radius;
-      this.boardWidth = boardWidth;
-      this.boardHeight = boardHeight;
-      this.direction = 1;
+  constructor(radius, boardWidth, boardHeight) {
+    this.radius = radius;
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+    this.direction = 1;
 
-      this.reset();
-    }
-
-    // loop runs 60 times a second.
-    render(svg) {
-        // draw ball
-        let circle = document.createElementNS(SVG_NS, 'circle');
-        circle.setAttributeNS(null, 'r', this.radius);
-        circle.setAttributeNS(null, 'cx', this.x); // control the ball movement
-        circle.setAttributeNS(null, 'cy', this.y); // control the ball movement
-        circle.setAttributeNS(null, 'fill', 'white');
-        svg.appendChild(circle);
+    this.reset();
   }
 
-//   This resets the ball after a player scores a goal.
+  // loop runs 60 times a second.
+  render(svg) {
+
+    this.x += this.vx;
+    this.y += this.vy;
+    // draw ball
+    let circle = document.createElementNS(SVG_NS, "circle");
+    circle.setAttributeNS(null, "r", this.radius);
+    circle.setAttributeNS(null, "cx", this.x); // control the ball movement
+    circle.setAttributeNS(null, "cy", this.y); // control the ball movement
+    circle.setAttributeNS(null, "fill", "white");
+    svg.appendChild(circle);
+  }
+
+  //   This resets the ball after a player scores a goal.
   reset() {
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
+    this.vy = Math.floor(Math.random() * 10 - 5);
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
-
-
 }
