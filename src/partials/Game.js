9f303327f3c17ba2519945d47dedf1c3e4399b93
Safenorsound <ijroback@gmail.com>
2018-08-22 10:16:09 -0700

    Repair CheckWinner function

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,21 +1,22 @@
-import { SVG_NS, KEYS } from '../settings';
 import Board from './Board';
+import { SVG_NS, KEYS } from '../settings';
 import Paddle from './Paddle';
 import Ball from './Ball';
 import Score from './Score';
-import winner from 'DeclareWinner';
+import Winner from './WinnerMessage';
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
     this.gameElement = document.getElementById(this.element);
-    // make a new board with a new game
+
     this.board = new Board(this.width, this.height);
-    // making players
+    // Making Players
     this.paddleWidth = 8;
     this.paddleHeight = 56;
     this.boardGap = 10;
+    this.autopause = true;
     this.player1 = new Paddle(
       this.height,
       this.paddleWidth,
@@ -24,7 +25,7 @@ export default class Game {
       (this.height - this.paddleHeight) / 2,
       KEYS.a,
       KEYS.z,
-      'player1'
+      'player 1'
     );
     this.player2 = new Paddle(
       this.height,
@@ -34,33 +35,44 @@ export default class Game {
       (this.height - this.paddleHeight) / 2,
       KEYS.up,
       KEYS.down,
-      'player2'
+      'player 2'
     );
     this.score1 = new Score(this.width / 2 - 50, 30, 30);
     this.score2 = new Score(this.width / 2 + 25, 30, 30);
-    this.winner = new winner(this.width / 2 - 50, this.height / 2 + 20, 50);
-    this.ball = new Ball(8, this.width, this.height);
+    // Position of the winner announcement.
+    this.winner = new Winner(this.width / 2 - 20, this.height / 2, 50);
+    //   Stretch Goal: creating more balls.
+    this.ball = new Ball(6, this.width, this.height);
     this.ball2 = new Ball(8, this.width, this.height);
-    // keydown for pausing game
+    this.ball3 = new Ball(10, this.width, this.height);
+    this.ball4 = new Ball(12, this.width, this.height);
+    //   Keydown for pausing game
     document.addEventListener('keydown', event => {
       switch (event.key) {
         case KEYS.spaceBar:
           this.pause = !this.pause;
-          break;
+        // this.pause = document.getElementById().;
       }
     });
-    // Other code goes here...end of constructor
   }
-  calculateWinner(player1, player2, svg) {
-    if (player1.score >= 10) {
+
+  checkWinner(p1, p2, svg) {
+    // Winning score
+    if (p1.score >= 2) {
+      // Pauses the game when a player wins.
       this.pause = !this.pause;
-      this.winner.render(svg, player1.name + ' Wins!');
-    } else if (player2.score >= 10) {
+      this.winner.render(svg, p1.name + 'Wins!!!');
+      // Winning score
+    } else if (p2.score >= 2) {
+      // Pauses the game when a player wins.
       this.pause = !this.pause;
-      this.winner.render(svg, player2.name + ' Wins!');
+      this.winner.render(svg, p2.name + 'Wins!!!');
     }
   }
+
   render() {
+    // if (this.pause) {
+    // if (this.newGame) {
     if (this.pause) {
       return;
     }
@@ -70,15 +82,19 @@ export default class Game {
     svg.setAttributeNS(null, 'height', this.height);
     svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
-    this.board.render(svg);
     // render the board
     this.board.render(svg);
     this.player1.render(svg);
     this.player2.render(svg);
     this.ball.render(svg, this.player1, this.player2);
     this.ball2.render(svg, this.player1, this.player2);
+    this.ball3.render(svg, this.player1, this.player2);
+    this.ball4.render(svg, this.player1, this.player2);
+
     this.score1.render(svg, this.player1.score);
     this.score2.render(svg, this.player2.score);
-    this.calculateWinner(this.player1, this.player2, svg);
+    this.checkWinner(this.player1, this.player2, svg);
   }
+  //   }
+  // }
 }
